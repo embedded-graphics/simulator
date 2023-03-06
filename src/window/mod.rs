11ec451e5743dc8b593b5c2f60615426fbe284e6
Grant Handy <granthandy@proton.me>
2023-03-06 12:19:47 -0700

    Improve Simulator CPU Usage up to 50%. (#45)
    * only create texture at start of program
    
    * limit window draw loop to 60Hz
    
    * allow user to set max fps

---
+++ src/window/mod.rs
@@ -0,0 +1,175 @@
+use std::{
+    env,
+    fs::File,
+    io::BufReader,
+    ops::Deref,
+    process, thread,
+    time::{Duration, Instant},
+};
+
+use embedded_graphics::{pixelcolor::Rgb888, prelude::*};
+
+use crate::{
+    display::SimulatorDisplay, output_image::OutputImage, output_settings::OutputSettings,
+};
+
+#[cfg(feature = "with-sdl")]
+mod sdl_window;
+
+#[cfg(feature = "with-sdl")]
+pub use sdl_window::{SdlWindow, SimulatorEvent};
+
+/// Simulator window
+#[allow(dead_code)]
+pub struct Window {
+    framebuffer: Option<OutputImage<Rgb888>>,
+    #[cfg(feature = "with-sdl")]
+    sdl_window: Option<SdlWindow>,
+    title: String,
+    output_settings: OutputSettings,
+    desired_loop_duration: Duration,
+    frame_start: Instant,
+}
+
+impl Window {
+    /// Creates a new simulator window.
+    pub fn new(title: &str, output_settings: &OutputSettings) -> Self {
+        Self {
+            framebuffer: None,
+            #[cfg(feature = "with-sdl")]
+            sdl_window: None,
+            title: String::from(title),
+            output_settings: output_settings.clone(),
+            desired_loop_duration: Duration::from_millis(1000 / output_settings.max_fps as u64),
+            frame_start: Instant::now(),
+        }
+    }
+
+    /// Updates the window.
+    pub fn update<C>(&mut self, display: &SimulatorDisplay<C>)
+    where
+        C: PixelColor + Into<Rgb888> + From<Rgb888>,
+    {
+        if let Ok(path) = env::var("EG_SIMULATOR_CHECK") {
+            let output = display.to_rgb_output_image(&self.output_settings);
+
+            let png_file = BufReader::new(File::open(path).unwrap());
+            let expected = image::load(png_file, image::ImageFormat::Png)
+                .unwrap()
+                .to_rgb8();
+
+            let png_size = Size::new(expected.width(), expected.height());
+
+            assert!(
+                output.size().eq(&png_size),
+                "display dimensions don't match PNG dimensions (display: {}x{}, PNG: {}x{})",
+                output.size().width,
+                output.size().height,
+                png_size.width,
+                png_size.height
+            );
+
+            assert!(
+                output
+                    .as_image_buffer()
+                    .as_raw()
+                    .eq(&expected.as_raw().deref()),
+                "display content doesn't match PNG file",
+            );
+
+            process::exit(0);
+        }
+
+        if let Ok(path) = env::var("EG_SIMULATOR_CHECK_RAW") {
+            let expected = SimulatorDisplay::load_png(path).unwrap();
+
+            assert!(
+                display.size().eq(&expected.size()),
+                "display dimensions don't match PNG dimensions (display: {}x{}, PNG: {}x{})",
+                display.size().width,
+                display.size().height,
+                expected.size().width,
+                expected.size().height
+            );
+
+            assert!(
+                display.pixels.eq(&expected.pixels),
+                "display content doesn't match PNG file",
+            );
+
+            process::exit(0);
+        }
+
+        if let Ok(path) = env::var("EG_SIMULATOR_DUMP") {
+            display
+                .to_rgb_output_image(&self.output_settings)
+                .save_png(path)
+                .unwrap();
+            process::exit(0);
+        }
+
+        if let Ok(path) = env::var("EG_SIMULATOR_DUMP_RAW") {
+            display
+                .to_rgb_output_image(&OutputSettings::default())
+                .save_png(path)
+                .unwrap();
+            process::exit(0);
+        }
+
+        #[cfg(feature = "with-sdl")]
+        {
+            if self.framebuffer.is_none() {
+                self.framebuffer = Some(OutputImage::new(display, &self.output_settings));
+            }
+
+            if self.sdl_window.is_none() {
+                self.sdl_window = Some(SdlWindow::new(display, &self.title, &self.output_settings));
+            }
+
+            let framebuffer = self.framebuffer.as_mut().unwrap();
+            let sdl_window = self.sdl_window.as_mut().unwrap();
+
+            framebuffer.update(display);
+            sdl_window.update(&framebuffer);
+        }
+
+        thread::sleep(
+            (self.frame_start + self.desired_loop_duration)
+                .saturating_duration_since(Instant::now()),
+        );
+
+        self.frame_start = Instant::now();
+    }
+
+    /// Shows a static display.
+    ///
+    /// This methods updates the window once and loops until the simulator window
+    /// is closed.
+    pub fn show_static<C>(&mut self, display: &SimulatorDisplay<C>)
+    where
+        C: PixelColor + Into<Rgb888> + From<Rgb888>,
+    {
+        self.update(&display);
+
+        #[cfg(feature = "with-sdl")]
+        'running: loop {
+            if self.events().any(|e| e == SimulatorEvent::Quit) {
+                break 'running;
+            }
+            thread::sleep(Duration::from_millis(20));
+        }
+    }
+
+    /// Returns an iterator of all captured SimulatorEvents.
+    ///
+    /// # Panics
+    ///
+    /// Panics if called before [`update`](Self::update) is called at least once.
+    #[cfg(feature = "with-sdl")]
+    pub fn events(&mut self) -> impl Iterator<Item = SimulatorEvent> + '_ {
+        self.sdl_window
+            .as_mut()
+            .unwrap()
+            .events(&self.output_settings)
+    }
+}
